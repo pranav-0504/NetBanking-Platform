977c8378c802ae3799841e8bdc9c6929c13df354
Pranav Aggarwal <pranav.aggarwal2004@gmail.com>
2026-08-04 02:06:28 +0530

    final backend domain url with https caddy firewall enabled

--- nexbank-frontend/src/environments/environment.development.ts
+++ nexbank-frontend/src/environments/environment.development.ts
@@ -1,7 +1,4 @@
 export const environment = {
-  // production: false,
-  // apiUrl: 'http://localhost:4000/api/v1',
   production: true,
-  apiUrl: 'http://20.219.16.75:4000/api/v1'
-
+  apiUrl: 'https://nexbank-api-vm.centralindia.cloudapp.azure.com/api/v1',
 };
--- nexbank-frontend/src/environments/environment.ts
+++ nexbank-frontend/src/environments/environment.ts
@@ -1,8 +1,14 @@
 // ! Deployed environment configuration for production for future reference. Do not use this file in development or testing environments.
+// export const environment = {
+
+//   production: true,
+//   // apiUrl: 'https://api.nexbank.com/api/v1'
+//   apiUrl: 'http://20.219.16.75:4000/api/v1'
+
+// };
+
+
 export const environment = {
-
   production: true,
-  // apiUrl: 'https://api.nexbank.com/api/v1'
-  apiUrl: 'http://20.219.16.75:4000/api/v1'
-
+  apiUrl: 'https://nexbank-api-vm.centralindia.cloudapp.azure.com/api/v1',
 };
